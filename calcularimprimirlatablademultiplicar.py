2693747463a9d9b8e87c9323a1cbae012d56e5b7
Braulio_ <braulioalexson@gmail.com>
2022-10-20 18:14:38 -0500

    Update calcularimprimirlatablademultiplicar.py

--- calcularimprimirlatablademultiplicar.py
+++ calcularimprimirlatablademultiplicar.py
@@ -1,1 +1,17 @@
-#include <iostream
+#include <iostream>
+
+/*Algoritmo detarea
+
+               Definir n Como Entero
+
+               Escribir «Ingresa un número»
+
+               leer n
+
+               para x = 1 Hasta 10 Con Paso 1 Hacer
+
+                              Escribir n,» x «,x,» = «,n*x
+
+               FinPara
+
+FinAlgoritmo*/ 
