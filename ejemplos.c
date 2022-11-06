f9e1a62bf6672a65157a8c09b9f96f3a4d14a3a2
Braulio_ <braulioalexson@gmail.com>
2022-11-06 12:28:24 -0600

    Update ejemplos.c

--- ejemplos.c
+++ ejemplos.c
@@ -22,4 +22,26 @@ int main(int argc, char const *argv[])
 
     system("pause");
     return 0;
+    
+    
+    
+    
+    
+    /* Console.WriteLine("¡hola time of Software!");
+            Console.ReadKey(); */        // el "readkey no pudes interactuar en la consola"
+
+            /* Console.Write("hola");    // el "write" no da salto de linea 
+            Console.Write("¿que haces?");
+            Console.WriteLine();        //el "writeline" da salto de linea y no tiene un mensaje hace un salto de linea para el otro codigo 
+            Console.WriteLine("hola");
+            Console.WriteLine("que haces?");
+            Console.ReadLine(); */      // el "readline" te permite escribir el la consola para almacenar informacion del usuario y interactuar, se usa para teclear datos que preguntas al usuario
+
+            /* int edadUsuario = 16;
+            string nombreUsuario = "Braulio";
+            Console.WriteLine("Hola me llamo" + nombreUsuario + "y tengo" + edadUsuario + "años.");
+            Console.WriteLine("hola me llamo {0} y tengo {1} años.",nombreUsuario,edadUsuario);
+            Console.WriteLine("Hola me llamo" + nombreUsuario+ ".\nTengo" + edadUsuario + "años.");
+            Console.WriteLine("hola me llamo \"{0}\" y tengo \"{1}\" años.",nombreUsuario,edadUsuario);
+            Console.ReadLine(); */
 }
